0aa5e20d9fa81e1845ef3bcd7074943f2a5e002c
AlexBibig <alexbibig@gmail.com>
2020-09-07 12:40:20 +0300

    test github pages

---
+++ build/precache-manifest.8e81e8e0f7f26f8bc82c6af8d7a7427c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a1c5c5b7c2a1cee357167e7107e908",
+    "url": "/TMS-React-toDoList/index.html"
+  },
+  {
+    "revision": "c0bbafdb32fe62411694",
+    "url": "/TMS-React-toDoList/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d4bb95a53444b443fb8",
+    "url": "/TMS-React-toDoList/static/css/main.a9c727ce.chunk.css"
+  },
+  {
+    "revision": "c0bbafdb32fe62411694",
+    "url": "/TMS-React-toDoList/static/js/2.5e489bde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TMS-React-toDoList/static/js/2.5e489bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4bb95a53444b443fb8",
+    "url": "/TMS-React-toDoList/static/js/main.ec5af6b5.chunk.js"
+  },
+  {
+    "revision": "6d90bdb27be2a8ac7e45",
+    "url": "/TMS-React-toDoList/static/js/runtime-main.7ababea1.js"
+  }
+]);
